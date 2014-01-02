ecf6b5672219fe26244de345c601e3ddbe63615f
Richard Allen <rallen@taoti.com>
2014-01-02 16:13:56 -0500

    js to check for adblock

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,10 +15,10 @@
         $(function(){
                 if($(".adsbygoogle").css('display')=="none") //use your ad's id here I have used Google Adense
                 {
-                    $('.adsbygoogle').replaceWith('<div class="adblocker">Hey I noticed you have an ad blocker.' +
+                    $('.adsbygoogle').replaceWith('<div class="adblocker">Hey, I noticed you have an ad blocker.' +
                         ' Well I\'m a developer paying off student loans still and every bit helps. I\'ve gone and ' +
                     'made these ads as un-intrusive as possible so if you could consider whitelisting this site ' +
-                '   it would be much appreciated!');
+                '   it would be much appreciated!</div>');
                     $('.adsbygoogle').css('display','block!important' );
                 }
         });
