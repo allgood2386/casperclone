8188bbd8e5168076900624fad9aa2b90db4c7acc
Richard Allen <rallen@taoti.com>
2014-01-02 14:57:33 -0500

    js to check for adblock

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,4 +15,13 @@
 
     });
 
+    $(function(){
+        setTimeout(function(){
+            if($("#google_ads_frame1").css('display')=="none") //use your ad's id here I have used Google Adense
+            {
+                $('body').html("We noticed you have an active Ad Blocker. Example.com is ad funded, we promise our ads are of high quality and are unobtrusive. The best help you could provide to keep us running, is to whitelist us in your ad blocker. Thanks!");
+            }
+        },3000);
+    });
+
 }(jQuery));
