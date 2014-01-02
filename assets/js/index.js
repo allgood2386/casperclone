203624f263daf52e342fd9ba2ae579f5093a6527
Richard Allen <rallen@taoti.com>
2014-01-02 15:36:58 -0500

    js to check for adblock

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,7 @@
         //$("#blog-logo").prependTo("#site-head-content");
         $(function(){
             setTimeout(function(){
-                if($("#google_ads_frame1").css('display')=="none") //use your ad's id here I have used Google Adense
+                if($(".addsbygoogle").css('display')=="none") //use your ad's id here I have used Google Adense
                 {
                     console.log('here3');
                     $('body').html("We noticed you have an active Ad Blocker. Example.com is ad funded, we promise our ads are of high quality and are unobtrusive. The best help you could provide to keep us running, is to whitelist us in your ad blocker. Thanks!");
