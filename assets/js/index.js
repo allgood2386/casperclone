8335c831b34ee4546284787ff9ab6e4961ab3dc6
Richard Allen <rallen@taoti.com>
2014-01-02 15:32:27 -0500

    js to check for adblock

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,16 +12,14 @@
         // for better relative/absolute positioning.
 
         //$("#blog-logo").prependTo("#site-head-content");
+        $(function(){
+            setTimeout(function(){
+                if($("#google_ads_frame1").css('display')=="none") //use your ad's id here I have used Google Adense
+                {
+                    $('body').html("We noticed you have an active Ad Blocker. Example.com is ad funded, we promise our ads are of high quality and are unobtrusive. The best help you could provide to keep us running, is to whitelist us in your ad blocker. Thanks!");
+                }
+            },3000);
+        });
 
     });
-
-    $(function(){
-        setTimeout(function(){
-            if($("#google_ads_frame1").css('display')=="none") //use your ad's id here I have used Google Adense
-            {
-                $('body').html("We noticed you have an active Ad Blocker. Example.com is ad funded, we promise our ads are of high quality and are unobtrusive. The best help you could provide to keep us running, is to whitelist us in your ad blocker. Thanks!");
-            }
-        },3000);
-    });
-
 }(jQuery));
